5c0bb13d0910ccad200bdb2605e47a90a6be65ce
Britt <brittcagnina@gmail.com>
2013-12-04 11:44:42 -0600

    doc path changed to relative

--- src/main/bitcoin/BlockLoader.java
+++ src/main/bitcoin/BlockLoader.java
@@ -6,15 +6,13 @@ import java.io.IOException;
 
 public class BlockLoader {
 
-	private static String WORKSPACE = "C:/Users/Stacey/workspace/";
-	
-	private static String DOC_DIR = "cryptocoins/src/main/doc/";
+	private static String DOC_DIR = "src/main/doc/";
 	
 	//to get our block info
 	public static String load(String fileName) {
 		String content = "";
 		try {
-			BufferedReader br = new BufferedReader(new FileReader(WORKSPACE + DOC_DIR + fileName));
+			BufferedReader br = new BufferedReader(new FileReader(DOC_DIR + fileName));
 			String currentLine;
 			while((currentLine = br.readLine()) != null) {
 				content += currentLine;
